a6d92a00131605afc2062606aef738d5bec8b94f
Janek2002 <jan.supierz02@gmail.com>
2023-03-13 12:28:11 +0100

    Step 2

---
+++ EX01_Pokemon/View/Converters/TypeToImageConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace EX01_Pokemon.View.Converters
+{
+    public class TypeToImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string type = value.ToString().ToLower();
+
+            return new BitmapImage(new Uri($"pack://application:,,,/Resources/Bg/{type}.jpg",UriKind.Absolute));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EX01_Pokemon/View/Converters/UrlToImageConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace EX01_Pokemon.View.Converters
+{
+    public class UrlToImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            BitmapImage image = new BitmapImage();
+
+            string path = value.ToString();
+            image.UriSource = new Uri(path);
+
+            return image;
+        }
+    
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
